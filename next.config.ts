d2a9d685df20eb75243e4a6820841d31a1249a2c
vende11s <wie.rzbinski9@gmail.com>
2026-06-26 20:00:29 +0200

    ci: add github actions workflow for pages deployment and base path config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/drill4" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/drill4/" : "",
 };
 
 export default nextConfig;
